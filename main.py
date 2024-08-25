bb223562ebbf8616301d3bc7072aff44b170b070
johnbhaigithub <149451117+johnbhaigithub@users.noreply.github.com>
2024-08-25 15:08:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pyrogram
+import pyrogram 3.11.3
 from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
